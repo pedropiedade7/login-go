b7a9557dd7410b0e687c65d25ba7f6348354f6c9
pedropiedade7 <pedropiedade854@gmail.com>
2024-05-20 23:45:32 -0300

    feat: serving static files and go templates

--- main.go
+++ main.go
@@ -1,31 +1,52 @@
 package main
 
 import (
-	"io"
-	"log"
+	"fmt"
+	"html/template"
 	"net/http"
-	"os"
-	"strings"
 )
 
-func StaticHandler(w http.ResponseWriter, r *http.Request) {
-	f, err := os.Open("static" + r.URL.Path)
+// func StaticHandler(w http.ResponseWriter, r *http.Request) {
+// 	f, err := os.Open("static" + r.URL.Path)
+// 	if err != nil {
+// 		log.Println(err)
+// 		return
+// 	}
+
+// 	if strings.HasSuffix(r.URL.Path, ".css") {
+// 		w.Header().Add("Content-Type", "text/css")
+// 	}
+
+// 	io.Copy(w, f)
+
+// }
+
+func ContactHandler(w http.ResponseWriter, r *http.Request) {
+	RenderTemplate(w, "contact")
+}
+
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	RenderTemplate(w, "index")
+}
+
+func RenderTemplate(w http.ResponseWriter, page string) {
+	tmpl, err := template.ParseFiles("templates/" + page + ".tmpl.html")
 	if err != nil {
-		log.Println(err)
+		fmt.Println(err)
 		return
 	}
-
-	if strings.HasSuffix(r.URL.Path, ".css") {
-		w.Header().Add("Content-Type", "text/css")
-	}
-
-	io.Copy(w, f)
-
+	tmpl.Execute(w, nil)
 }
 
 func main() {
 
-	http.HandleFunc("/", StaticHandler)
+	static := http.Dir("static")
+	staticHandler := http.FileServer(static)
+
+	http.HandleFunc("/", HomeHandler)
+	http.HandleFunc("/contact", ContactHandler)
+
+	http.Handle("/static/", http.StripPrefix("/static/", staticHandler))
 
 	http.ListenAndServe(":3000", nil)
 }
