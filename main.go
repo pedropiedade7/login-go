93dee22448921f0925088beedd1d72201581bb19
pedropiedade7 <pedropiedade854@gmail.com>
2024-05-21 23:19:59 -0300

    feat(aula 4): templates html

--- main.go
+++ main.go
@@ -21,6 +21,8 @@ import (
 
 // }
 
+var cache map[string]*template.Template
+
 func ContactHandler(w http.ResponseWriter, r *http.Request) {
 	RenderTemplate(w, "contact")
 }
@@ -29,22 +31,44 @@ func HomeHandler(w http.ResponseWriter, r *http.Request) {
 	RenderTemplate(w, "index")
 }
 
+func AboutHandler(w http.ResponseWriter, r *http.Request) {
+	RenderTemplate(w, "about")
+}
+
 func RenderTemplate(w http.ResponseWriter, page string) {
-	tmpl, err := template.ParseFiles("templates/" + page + ".tmpl.html")
-	if err != nil {
-		fmt.Println(err)
-		return
+	env := "dev"
+
+	var t *template.Template
+	var err error
+
+	_, exists := cache[page]
+
+	if !exists || env == "dev" {
+		t, err = template.ParseFiles(
+			"templates/"+page+".tmpl.html", "templates/base.layout.tmpl.html")
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		cache[page] = t
+	} else {
+		fmt.Println("Cache Hit")
+		t = cache[page]
 	}
-	tmpl.Execute(w, nil)
+
+	t.Execute(w, nil)
 }
 
 func main() {
+
+	cache = make(map[string]*template.Template)
 
 	static := http.Dir("static")
 	staticHandler := http.FileServer(static)
 
 	http.HandleFunc("/", HomeHandler)
 	http.HandleFunc("/contact", ContactHandler)
+	http.HandleFunc("/about", AboutHandler)
 
 	http.Handle("/static/", http.StripPrefix("/static/", staticHandler))
 
